e824788cef31edba39f0c711e918b1c7dc376709
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2023-04-07 15:35:10 +0700

    Fix Error

--- K6/Assets/Script/Attack.cs
+++ K6/Assets/Script/Attack.cs
@@ -6,27 +6,14 @@ using UnityEngine.SceneManagement;
 public class Attack : MonoBehaviour
 {
 
-   public HealthSystem playerHp;
-   public int damage = 1;
-   private bool present;
+    public HealthSystem playerHp;
+    public int damage = 1;
 
-   private void OnCollisionEnter2D(Collision2D collision)
-   {
-      //Jika platform menabrak player, hp player dikurangi sejumlah damage
-      if (collision.gameObject.tag == "Player") {
-         this.present = true;
-         this.playerHp.ReduceHealth(this.damage);
-      }
-   }
-
-   private void OnCollisionExit2D(Collision2D collision) {
-      this.present = false;
-   }
-
-   IEnumerator DamageCoroutine() {
-      while (this.present) {
-         this.playerHp.ReduceHealth(this.damage);
-         yield return new WaitForSeconds(1);
-      }
-   }
+    private void OnCollisionEnter2D(Collision2D collision)
+     {
+        //Jika platform menabrak player, hp player dikurangi sejumlah damage
+         if (collision.gameObject.tag == "Player") {
+            this.playerHp.ReduceHealth(this.damage);
+         }
+     }
 }
